0dd0a722944b22aa378db34c5ee110184f1e928a
VeryBarry <barry.a.daniels@gmail.com>
2016-10-12 11:02:34 -0400

    uses db

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 import org.h2.tools.Server;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
@@ -10,5 +10,33 @@ public class Main {
 
         Server.createWebServer().start();
 
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (id IDENTITY, name VARCHAR, score INT, health DOUBLE, is_alive BOOLEAN)");
+        stmt.execute("INSERT INTO players VALUES (NULL, 'Alice', 10, 100, true)");
+        stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
+        stmt.execute("DELETE FROM players WHERE name = 'Alice'");
+
+        String name = "Charlie";
+
+        //bad way (open for sql injections)
+        stmt.execute(String.format("INSERT INTO players VALUES (NULL, '%s', 10, 100, true)", name));
+
+        //good
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (NULL, ?, 10, 100, true)");
+        //1 is the ? location since there is only 1 it is the first (starts at 1)
+        stmt2.setString(1, name);
+        stmt2.execute();
+
+        PreparedStatement stmt3 = conn.prepareStatement("SELECT * FROM players WHERE name = ?");
+        stmt3.setString(1, name);
+        ResultSet results = stmt3.executeQuery();
+        while (results.next()) {
+            int id = results.getInt("id");
+            int score = results.getInt("score");
+            double health = results.getDouble("health");
+            boolean isAlive = results.getBoolean("is_alive");
+            System.out.printf("%s %s %s %s %s\n", name, id, score, health, isAlive);
+        }
     }
 }
